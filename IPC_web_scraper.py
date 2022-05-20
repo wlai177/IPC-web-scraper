b3c8ea7674891fe9eaa1218c72e6b18be58a6294
Wesley Lai <wesley.lai@zoho.com>
2022-05-19 23:26:51 -0700

    Add export to CSV. Improve parse function.

--- IPC_web_scraper.py
+++ IPC_web_scraper.py
@@ -1,18 +1,26 @@
 from bs4 import BeautifulSoup
-import requests
-
-print("*** Program started ***")
-
-start_page = 0
-end_page = 2    # Use 1375 for the real end page
+import requests, csv
 
 def parseLocation(location):
     location_list = location.split(", ", 2)
     if 2 == len(location_list):
         location_list.append(location_list[1])
         location_list[1] = ""
+    elif 1 == len(location_list):
+        location_list.append("")
+        location_list.append(location_list[0])
+        location_list[0] = ""
     return location_list
 
+
+print("*** Program started ***")
+
+start_page = 0
+end_page = 10    # Use 1375 for the real end page
+
+f = open('export.csv', 'w', newline='')
+writer = csv.writer(f)
+writer.writerow(["Company", "City", "State", "Country", "Number of Certs"])
 
 for page in range (start_page, end_page):
 
@@ -43,9 +51,13 @@ for page in range (start_page, end_page):
 
             location = parseLocation(location)
 
-            print(location)
+            # print(location)
+            print(name, location, num_cert)
+
+            writer.writerow([name, location[0], location[1], location[2], num_cert])
             
     except Exception as e:
         print(e)
 
+f.close()
 print("*** Program finished ***")
